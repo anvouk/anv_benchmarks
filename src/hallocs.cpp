667e1267b863b2964ee612ce896907496cb0cf8f
Andrea Vouk <andrew.vouk@gmail.com>
2023-05-01 20:28:46 +0200

    Add stb halloc to benchmarks

--- src/hallocs.cpp
+++ src/hallocs.cpp
@@ -3,8 +3,8 @@
 #define HALLOC_IMPLEMENTATION
 #include "halloc.h"
 
-#define ANV_HALLOC_IMPLEMENTATION
-#include "anv_halloc.h"
+#define STB_ALLOC_IMPLEMENTATION
+#include "stb_alloc.h"
 
 #undef ensure
 #define ensure(x)                                                                           \
@@ -44,16 +44,16 @@ static void BM_h_halloc_new_free_root(benchmark::State& state)
 }
 BENCHMARK(BM_h_halloc_new_free_root);
 
-static void BM_h_anv_halloc_new_free_root(benchmark::State& state)
+static void BM_h_stb_new_free_root(benchmark::State& state)
 {
     test_t* root = NULL;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(root = (test_t*)anvh_alloc(NULL, sizeof(test_t), 0));
+        benchmark::DoNotOptimize(root = (test_t*)stb_malloc(NULL, sizeof(test_t)));
         ensure(root);
-        anvh_free(root);
+        stb_free(root);
     }
 }
-BENCHMARK(BM_h_anv_halloc_new_free_root);
+BENCHMARK(BM_h_stb_new_free_root);
 
 static void BM_h_halloc_new_children(benchmark::State& state)
 {
@@ -69,18 +69,18 @@ static void BM_h_halloc_new_children(benchmark::State& state)
 }
 BENCHMARK(BM_h_halloc_new_children);
 
-static void BM_h_anv_halloc_new_children(benchmark::State& state)
+static void BM_h_stb_new_children(benchmark::State& state)
 {
-    test_t* root = (test_t*)anvh_alloc(NULL, sizeof(test_t), state.max_iterations);
+    test_t* root = (test_t*)stb_malloc(NULL, sizeof(test_t));
     ensure(root);
     test_t* child = NULL;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(child = (test_t*)anvh_alloc(root, sizeof(test_t), state.range(0)));
+        benchmark::DoNotOptimize(child = (test_t*)stb_malloc(root, sizeof(test_t)));
         ensure(child);
     }
-    anvh_free(root);
+    stb_free(root);
 }
-BENCHMARK(BM_h_anv_halloc_new_children)->Arg(0)->Arg(8)->Arg(64)->Arg(512)->Arg(4096)->Arg(8192);
+BENCHMARK(BM_h_stb_new_children);
 
 static void BM_h_halloc_alloc_free_with_children(benchmark::State& state)
 {
@@ -98,32 +98,17 @@ static void BM_h_halloc_alloc_free_with_children(benchmark::State& state)
 }
 BENCHMARK(BM_h_halloc_alloc_free_with_children)->Arg(8)->Arg(64)->Arg(512)->Arg(4096)->Arg(8192);
 
-static void BM_h_anv_halloc_alloc_free_with_children(benchmark::State& state)
+static void BM_h_stb_alloc_free_with_children(benchmark::State& state)
 {
     test_t* child = NULL;
     for (auto _ : state) {
-        test_t* root = (test_t*)anvh_alloc(NULL, sizeof(test_t), state.max_iterations);
+        test_t* root = (test_t*)stb_malloc(NULL, sizeof(test_t));
         ensure(root);
         for (size_t i = 0; i < state.range(0); ++i) {
-            benchmark::DoNotOptimize(child = (test_t*)anvh_alloc(root, sizeof(test_t), 0));
+            benchmark::DoNotOptimize(child = (test_t*)stb_malloc(root, sizeof(test_t)));
             ensure(child);
         }
-        anvh_free(root);
+        stb_free(root);
     }
 }
-BENCHMARK(BM_h_anv_halloc_alloc_free_with_children)->Arg(8)->Arg(64)->Arg(512)->Arg(4096)->Arg(8192);
-
-static void BM_h_anv_halloc_alloc_free_with_children_children(benchmark::State& state)
-{
-    test_t* child = NULL;
-    for (auto _ : state) {
-        test_t* root = (test_t*)anvh_alloc(NULL, sizeof(test_t), state.max_iterations);
-        ensure(root);
-        for (size_t i = 0; i < state.range(0); ++i) {
-            benchmark::DoNotOptimize(child = (test_t*)anvh_alloc(root, sizeof(test_t), state.range(0)));
-            ensure(child);
-        }
-        anvh_free(root);
-    }
-}
-BENCHMARK(BM_h_anv_halloc_alloc_free_with_children_children)->Arg(8)->Arg(64)->Arg(512)->Arg(4096)->Arg(8192);
+BENCHMARK(BM_h_stb_alloc_free_with_children)->Arg(8)->Arg(64)->Arg(512)->Arg(4096)->Arg(8192);
